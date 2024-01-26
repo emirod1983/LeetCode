7c2bc2072cee57263abdc79b2f99c90f18754e29
emirod1983 <emirod1983@gmail.com>
2024-01-26 16:22:44 -0300

    Added alternative solution for TwoSum

--- LeetCode-Tests/0001-0050/_01_TwoSum_Tests.cs
+++ LeetCode-Tests/0001-0050/_01_TwoSum_Tests.cs
@@ -24,5 +24,21 @@ namespace LeetCode_Tests
                 result.Should().ContainInOrder(expected);
             }
         }
+
+
+        public class TheMethod_TwoSum_Dict : _01_TwoSum_Tests
+        {
+            [Theory]
+            [InlineData(new[] { 2, 7, 11, 15 }, 9, new[] { 0, 1 })]
+            [InlineData(new[] { 3, 2, 4 }, 6, new[] { 1, 2 })]
+            [InlineData(new[] { 3, 3 }, 6, new[] { 0, 1 })]
+            [InlineData(new[] { 2, 2, 4 }, 6, new[] { 1, 2 })]
+            public void ShouldValidateTwoSum(int[] nums, int target, int[] expected)
+            {
+                var result = this.Sut.TwoSum_Dict(nums, target);
+
+                result.Should().ContainInOrder(expected);
+            }
+        }
     }
 }
--- LeetCode/0001-0050/01-TwoSum.cs
+++ LeetCode/0001-0050/01-TwoSum.cs
@@ -18,7 +18,23 @@ namespace LeetCode._0001_0050
                 }
             }
 
-            return new int[] { -1, -1 };
+            return new int[] { };
+        }
+
+        // Alternative solution using Dictionary
+        public int[] TwoSum_Dict(int[] nums, int target)
+        {
+            var dic = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dic.ContainsKey(nums[i]))
+                    return new int[] { dic[nums[i]], i };
+                else
+                    dic[target - nums[i]] = i;
+            }
+
+            return new int[] { };
         }
     }
 }
